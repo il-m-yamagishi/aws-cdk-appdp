b3a83d7105ad47d10f5c7cbffc90be9f55c21188
m-yamagishi <m-yamagishi@infiniteloop.co.jp>
2024-08-08 20:56:26 +0900

    chore: Make public

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,5 @@
 import { awscdk } from 'projen';
+import { NpmAccess } from 'projen/lib/javascript';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Masaru Yamagishi',
   authorAddress: 'm-yamagishi@infiniteloop.co.jp',
@@ -7,7 +8,9 @@ const project = new awscdk.AwsCdkConstructLibrary({
   jsiiVersion: '~5.4.0',
   name: 'aws-cdk-appdp',
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/m-yamagishi/aws-cdk-appdp.git',
+  repositoryUrl: 'https://github.com/il-m-yamagishi/aws-cdk-appdp.git',
+  npmProvenance: true,
+  npmAccess: NpmAccess.PUBLIC,
 
   // deps: [],                /* Runtime dependencies of this module. */
   description: 'AWS CDK Typical application deployment for AWS Fargate, alternative to AWS CDK Pipelines.', /* The description is just a string that helps people understand the purpose of the package. */
