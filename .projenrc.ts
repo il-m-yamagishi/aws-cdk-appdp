05305f2f72d9301733cf41784e3dd775ede2f0f0
yamayuski <m.yamagishi90+git@gmail.com>
2024-09-09 17:28:53 +0900

    chore: Fix projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,7 +2,7 @@ import { awscdk } from 'projen';
 import { NpmAccess } from 'projen/lib/javascript';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Masaru Yamagishi',
-  authorAddress: 'm-yamagishi@infiniteloop.co.jp',
+  authorAddress: 'm.yamagishi90+git@gmail.com',
   cdkVersion: '2.151.0',
   defaultReleaseBranch: 'main',
   jsiiVersion: '~5.4.0',
@@ -11,13 +11,8 @@ const project = new awscdk.AwsCdkConstructLibrary({
   repositoryUrl: 'https://github.com/yamayuski/aws-cdk-appdp.git',
   npmProvenance: true,
   npmAccess: NpmAccess.PUBLIC,
-
-  // deps: [],                /* Runtime dependencies of this module. */
+  experimentalIntegRunner: false,
   description: 'AWS CDK Typical application deployment for AWS Fargate, alternative to AWS CDK Pipelines.', /* The description is just a string that helps people understand the purpose of the package. */
-  devDeps: [
-    '@aws-cdk/integ-runner@latest',
-    '@aws-cdk/integ-tests-alpha@latest',
-  ],
   packageName: '@m-yamagishi/aws-cdk-appdp',
 });
 project.synth();
